800559013108f8b5d9e32ca7eb29f090c659d656
goFrendiAsgard <goFrendiAsgard@gmail.com>
2012-09-07 12:08:47 +0700

    add implementation of some methods, fix some syntax errors

--- Feature_Extractor.py
+++ Feature_Extractor.py
@@ -1,10 +1,23 @@
-# ========== Feature Extractor By Using Grammatical Evolution With =========== # 
-# ====================== Multi Fitness Calculation ============================#
-#                                                                              #
-#  Author : Go Frendi Gunawan                                                  #
-#  Description : Proofing my final project hypothesis about feature extraction #
-#                                                                              #
-# ============================================================================ #
+#!/usr/bin/env python
+
+# ==================================0000====================================== #
+
+""" 
+    Feature_Extractor.py: 
+    A program to proof my hypothesis about feature extraction 
+    by using grammatical evolution with multiple fitness value
+"""
+
+__author__      = "Go Frendi Gunawan"
+__copyright__   = "Copyright 2012, Planet Earth, Solar System"
+__credits__     = ["Go Frendi Gunawan"]
+__license__     = "GPL"
+__version__     = "0.0.2"
+__maintainer__  = "Go Frendi Gunawan"
+__status__      = "Development"
+
+# ==================================0000====================================== #
+
 
 import random
 random.seed(1567)
@@ -12,17 +25,17 @@ random.seed(1567)
 # This is the class declaration
 class Feature_Extractor(object):
     
-    # constructor
+    # ------------------------------ constructor ----------------------------- #
     def __init__(self):
-        self.genotype_dictionary = []
-        self.fenotype_fitness = []
+        self.genotype_dictionary = {}
+        self.phenotype_fitness = {}
         self.original_features = []
         self.grammar = {}
         self.start_node = ''
         self.classes = []
         self.data = []
     
-    # setters ==================================================================
+    # -------------------------------- setters ------------------------------- #
     
     # set features
     def set_features(self, features):
@@ -44,7 +57,39 @@ class Feature_Extractor(object):
     def set_data(self, data):
         self.data = data 
     
-    # methods ==================================================================
+    # -------------------------------- methods ------------------------------- #
+    
+    # return phenotype of the gene
+    def _transform(self, gene):
+        pass
+    
+    # calculate the fitness value of a phenotype
+    def _calculate_fitness(self, phenotype):
+        for classes in self.classes:
+            for record in self.data:
+                pass
+    
+    # register genotype into genotype_dictionary and fenotype_fitness
+    def _register_genotype(self, gene):
+    
+        # declare phenotype
+        phenotype = ''        
+        
+        # if the gene is not exists is genotype_dictionary, then calculate
+        # phenotype, and register it in genotype_dictionary.
+        # else, just take phenotype value from genotype_dictionary
+        try:
+            phenotype = self.genotype_dictionary[gene]
+        except:
+            phenotype = self._transform(gene)
+            self.genotype_dictionary[gene] = phenotype            
+        
+        # register the phenotype fitness
+        try:
+            test = self.phenotype_fitness[phenotype]
+        except:
+            self.phenotype_fitness[phenotype] = 
+                self._calculate_fitness(phenotype)
     
     # mutation
     def _mutation(self, gene):
@@ -58,10 +103,32 @@ class Feature_Extractor(object):
     def _new_individu(self):
         pass
     
+    # new generation
     def _new_generation(self):
         pass
+    
+    def process(self):
+        # the original features has a VIP chance to be in competition
+        for i in range(len(self.original_features)):
+            phenotype = self.original_features[i]
+            try:
+                test = self.phenotype_fitness[phenotype]
+            except:
+                self.phenotype_fitness[phenotype] = 
+                    self._calculate_fitness(phenotype)
+            
+                
+        # other processes
+        
+        # show the results
+        print('Genotype-Phenotype Dictionary :')
+        print(self.genotype_dictionary)
+        print('Fitness of All Phenotypes :')
+        print(self.phenotype_fitness)
+        
+        
  
-# Test case ====================================================================
+# =============================== TEST CASE ================================== #
 if __name__ == '__main__':
  
     # declare parameters
@@ -74,7 +141,7 @@ if __name__ == '__main__':
             '<function>(<expr>)'],
         '<value>' : ['<feature>', '<number>'],
         '<feature>' : features,
-        '<number>' : ['<digit>.<digit>', '<digit>']
+        '<number>' : ['<digit>.<digit>', '<digit>'],
         '<digit>' : ['<digit><digit>', '0', '1', '2', '3', '4', '5', '6', '7',
             '8', '9'],
         '<opreator>' : ['+', '-', '*', '/'],
