f0088873b5b1d59386689bf12f39faab9e9286e9
goFrendiAsgard <goFrendiAsgard@gmail.com>
2012-09-07 16:33:14 +0700

    add implementation of transformation

--- Feature_Extractor.py
+++ Feature_Extractor.py
@@ -27,6 +27,7 @@ class Feature_Extractor(object):
     
     # ------------------------------ constructor ----------------------------- #
     def __init__(self):
+        # attributes 
         self.genotype_dictionary = {}
         self.phenotype_fitness = {}
         self.original_features = []
@@ -34,6 +35,7 @@ class Feature_Extractor(object):
         self.start_node = ''
         self.classes = []
         self.data = []
+        self.gene_length = 50
     
     # -------------------------------- setters ------------------------------- #
     
@@ -59,9 +61,83 @@ class Feature_Extractor(object):
     
     # -------------------------------- methods ------------------------------- #
     
+    # binary to decimal conversion
+    def _to_decimal(binary_number):
+        # manual way is verbose and not elegant, but in this case speed is
+        # very important.
+        binary_dictionary = {
+            '0'   : 0,   '1'   : 1,   '00'  : 0,   '01'  : 1,
+            '10'  : 2,   '11'  : 3,   '000' : 0,   '001' : 1,
+            '010' : 2,   '011' : 3,   '100' : 4,   '101' : 5,
+            '110' : 6,   '111' : 7
+        }        
+        try:
+            number = binary_dictionary[binary_number]
+            return number
+        except:
+            # since power operation is expensive, it should only be used in 
+            # very special case
+            multiplier = 0
+            number = 0
+            for el in binary_number[0:-1]:
+                number += int(el) * (2**multiplier)
+                multiplier += 1
+            return number
+    
+    # return how many binary digit needed to represent a decimal number
+    def _get_binary_digit_count(self, number):
+        # manual way is verbose and not elegant, but in this case speed is
+        # very important. 
+        power_dictionary = {
+            '2'   : 1,   '4'   : 1,   '8'   : 3,   '16'  : 4,
+            '32'  : 5,   '64'  : 6,   '128' : 7,   '256' : 8
+        }     
+        for key in power_dictionary:
+            if number<int(key):
+                return power_dictionary[key]
+            
+        # since power operation is expensive, it should only be used in very
+        # special case
+        i = 8
+        while number < 2**i :
+            i += 1
+        return i
+        
+    
     # return phenotype of the gene
     def _transform(self, gene):
-        pass
+        depth = 10
+        gene_index = 0
+        newExpr = self.start_node
+        # for each level
+        for level in xrange(depth):
+            i=0
+            # parse every character in the expr
+            while i<len(newExpr):
+                find = False
+                for key in self.grammar:
+                    # if there is a keyword in the grammar, replace it
+                    # with rule in production
+                    if (expr[i:i+len(key)] == key):
+                        find = True
+                        # count how many transformation possibility exists
+                        possibility = len(self.grammar[key])
+                        # calculate how many binary digit needed to represent
+                        # the possibilities
+                        digit_needed = self.get_binary_digit(possibility)
+                        used_gene = gene[gene_index:digit_needed]
+                        # cyclic gene
+                        if(gene_index+used_gene)>len(gene):
+                            gene_index = 0
+                        else: 
+                            gene_index += self.get_binary_digit(possibility)
+                        rule_index = self._to_decimal(used_gene)
+                        newExpr += self.grammar[key][rule_index]
+                        i+= len(key)-1
+                if not find:
+                    newExpr += expr[i:i+1]
+                i += 1
+        return newExpr
     
     # calculate the fitness value of a phenotype
     def _calculate_fitness(self, phenotype):
@@ -88,16 +164,7 @@ class Feature_Extractor(object):
         try:
             test = self.phenotype_fitness[phenotype]
         except:
-            self.phenotype_fitness[phenotype] = 
-                self._calculate_fitness(phenotype)
-    
-    # mutation
-    def _mutation(self, gene):
-        pass
-    
-    # crossover
-    def _crossover(self, gene_1, gene_2):
-        pass
+            self.phenotype_fitness[phenotype] = self._calculate_fitness(phenotype)
     
     # new individu
     def _new_individu(self):
@@ -114,8 +181,7 @@ class Feature_Extractor(object):
             try:
                 test = self.phenotype_fitness[phenotype]
             except:
-                self.phenotype_fitness[phenotype] = 
-                    self._calculate_fitness(phenotype)
+                self.phenotype_fitness[phenotype] = self._calculate_fitness(phenotype)
             
                 
         # other processes
