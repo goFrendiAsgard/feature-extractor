892d4a5c08a1a363ee87a5f2fce25ec5b4f8fc41
goFrendiAsgard <goFrendiAsgard@gmail.com>
2012-09-08 09:26:20 +0700

    finishing transformation implementation

--- Feature_Extractor.py
+++ Feature_Extractor.py
@@ -12,7 +12,7 @@ __author__      = "Go Frendi Gunawan"
 __copyright__   = "Copyright 2012, Planet Earth, Solar System"
 __credits__     = ["Go Frendi Gunawan"]
 __license__     = "GPL"
-__version__     = "0.0.2"
+__version__     = "0.0.3"
 __maintainer__  = "Go Frendi Gunawan"
 __status__      = "Development"
 
@@ -20,9 +20,12 @@ __status__      = "Development"
 
 
 import random
-random.seed(1567)
+random.seed(15)
 
-# This is the class declaration
+# ============================================================================ # 
+# =========================== FEATURE EXTRACTOR ============================== #
+# ============================================================================ #
+
 class Feature_Extractor(object):
     
     # ------------------------------ constructor ----------------------------- #
@@ -36,6 +39,7 @@ class Feature_Extractor(object):
         self.classes = []
         self.data = []
         self.gene_length = 50
+        self.population_size = 200
     
     # -------------------------------- setters ------------------------------- #
     
@@ -62,21 +66,23 @@ class Feature_Extractor(object):
     # -------------------------------- methods ------------------------------- #
     
     # binary to decimal conversion
-    def _to_decimal(binary_number):
-        # manual way is verbose and not elegant, but in this case speed is
-        # very important.
+    def _to_decimal(self, binary_number):
+        # manual way is verbose and not elegant, but in this case speed is very important.
         binary_dictionary = {
             '0'   : 0,   '1'   : 1,   '00'  : 0,   '01'  : 1,
             '10'  : 2,   '11'  : 3,   '000' : 0,   '001' : 1,
             '010' : 2,   '011' : 3,   '100' : 4,   '101' : 5,
-            '110' : 6,   '111' : 7
+            '110' : 6,   '111' : 7,   '0000': 0,   '0001': 1,
+            '0010': 2,   '0011': 3,   '0100': 4,   '0101': 5,
+            '0110': 6,   '0111': 7,   '1000': 8,   '1001': 9,
+            '1010': 10,  '1011':11,   '1100': 12,  '1101': 13,
+            '1110': 14,  '1111':15
         }        
         try:
             number = binary_dictionary[binary_number]
             return number
         except:
-            # since power operation is expensive, it should only be used in 
-            # very special case
+            # since power operation is expensive, it should only be used in very special case
             multiplier = 0
             number = 0
             for el in binary_number[0:-1]:
@@ -86,18 +92,16 @@ class Feature_Extractor(object):
     
     # return how many binary digit needed to represent a decimal number
     def _get_binary_digit_count(self, number):
-        # manual way is verbose and not elegant, but in this case speed is
-        # very important. 
-        power_dictionary = {
-            '2'   : 1,   '4'   : 1,   '8'   : 3,   '16'  : 4,
-            '32'  : 5,   '64'  : 6,   '128' : 7,   '256' : 8
-        }     
-        for key in power_dictionary:
-            if number<int(key):
-                return power_dictionary[key]
+        # manual way is verbose and not elegant, but in this case speed is very important. 
+        power_array = [
+            [2   , 1 ], [4   , 2 ], [8   , 3 ], [16  , 4 ], [32  , 5 ],
+            [64  , 6 ], [128 , 7 ], [256 , 8 ], [512 , 9 ], [1024, 10],
+        ]
+        for i in range(len(power_array)):
+            if number<=int(power_array[i][0]):
+                return power_array[i][1]
             
-        # since power operation is expensive, it should only be used in very
-        # special case
+        # since power operation is expensive, it should only be used in very special case
         i = 8
         while number < 2**i :
             i += 1
@@ -108,36 +112,36 @@ class Feature_Extractor(object):
     def _transform(self, gene):
         depth = 10
         gene_index = 0
-        newExpr = self.start_node
+        expr = self.start_node
         # for each level
         for level in xrange(depth):
             i=0
+            new_expr = ''
             # parse every character in the expr
-            while i<len(newExpr):
-                find = False
+            while i<len(expr):                
+                found = False
                 for key in self.grammar:
-                    # if there is a keyword in the grammar, replace it
-                    # with rule in production
+                    # if there is a keyword in the grammar, replace it with rule in production
                     if (expr[i:i+len(key)] == key):
-                        find = True
+                        found = True
                         # count how many transformation possibility exists
                         possibility = len(self.grammar[key])
-                        # calculate how many binary digit needed to represent
-                        # the possibilities
-                        digit_needed = self.get_binary_digit(possibility)
-                        used_gene = gene[gene_index:digit_needed]
-                        # cyclic gene
-                        if(gene_index+used_gene)>len(gene):
+                        # how many binary digit needed to represent the possibilities
+                        digit_needed = self._get_binary_digit_count(possibility)
+                        used_gene = gene[gene_index:gene_index+digit_needed]
+                        # if end of gene, then start over from the beginning
+                        if(gene_index+len(used_gene))>len(gene):
                             gene_index = 0
                         else: 
-                            gene_index += self.get_binary_digit(possibility)
-                        rule_index = self._to_decimal(used_gene)
-                        newExpr += self.grammar[key][rule_index]
+                            gene_index += digit_needed                        
+                        rule_index = self._to_decimal(used_gene) % possibility
+                        new_expr += self.grammar[key][rule_index]
                         i+= len(key)-1
-                if not find:
-                    newExpr += expr[i:i+1]
+                if not found:
+                    new_expr += expr[i:i+1]
                 i += 1
-        return newExpr
+            expr = new_expr
+        return expr
     
     # calculate the fitness value of a phenotype
     def _calculate_fitness(self, phenotype):
@@ -166,13 +170,24 @@ class Feature_Extractor(object):
         except:
             self.phenotype_fitness[phenotype] = self._calculate_fitness(phenotype)
     
-    # new individu
+    # return new individu's genotype (binary string)
     def _new_individu(self):
-        pass
+        gene = ''
+        for i in range(self.gene_length):
+            number = random.randint(0,10)
+            if number<5:
+                gene += '0'
+            else:
+                gene += '1'
+        return gene
     
-    # new generation
+    # return new generation (array of binary string)
     def _new_generation(self):
-        pass
+        generation = []
+        for i in range(self.population_size):
+            individu = self._new_individu()
+            generation.append(individu)
+        return generation
     
     def process(self):
         # the original features has a VIP chance to be in competition
@@ -184,17 +199,19 @@ class Feature_Extractor(object):
                 self.phenotype_fitness[phenotype] = self._calculate_fitness(phenotype)
             
                 
-        # other processes
+        # new generation
+        generation = self._new_generation()
+        for i in range(len(generation)):
+            self._register_genotype(generation[i])
         
         # show the results
-        print('Genotype-Phenotype Dictionary :')
-        print(self.genotype_dictionary)
         print('Fitness of All Phenotypes :')
         print(self.phenotype_fitness)
         
         
- 
+# ============================================================================ # 
 # =============================== TEST CASE ================================== #
+# ============================================================================ #
 if __name__ == '__main__':
  
     # declare parameters
@@ -208,9 +225,11 @@ if __name__ == '__main__':
         '<value>' : ['<feature>', '<number>'],
         '<feature>' : features,
         '<number>' : ['<digit>.<digit>', '<digit>'],
-        '<digit>' : ['<digit><digit>', '0', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9'],
-        '<opreator>' : ['+', '-', '*', '/'],
+        '<digit>' : [
+            '<digit><digit>', 
+            '0', '1', '2', '3', '4', 
+            '5', '6', '7', '8', '9'],
+        '<operator>' : ['+', '-', '*', '/'],
         '<function>' : ['sin', 'cos', 'tan', 'abs']
     }
     classes = ['A', 'B', 'C']
