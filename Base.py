00d625beaab701623f336272061a5d7ff80f760d
goFrendiAsgard <goFrendiAsgard@gmail.com>
2012-10-29 11:03:08 +0700

    finsihing base

--- Base.py
+++ Base.py
@@ -4,6 +4,7 @@ Created on Oct 23, 2012
 @author: gofrendi
 '''
 
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -67,55 +68,117 @@ class GA_Base(object):
         self._mutation_rate = 0.3
         self._crossover_rate = 0.3
         self._new_rate = 0.2
+        self._individual_benchmark_rank = {} # rank of individual in current generation
+        self._individual_total_fitness = {} # total fitness of individuals
     
-    def take_individual(self,generation_index):
-        return {}
+    def _sort(self,benchmark):
+        '''
+        Quick sorting self._individual_benchmark_rank
+        '''
+        lst = self._individual_benchmark_rank[benchmark]
+        pivot = lst[0]
+        lesser = self._sort([x for x in lst[1:] if x['fitness'] < pivot['fitness']])
+        greater = self._sort([x for x in lst[1:] if x['fitness'] >= pivot['fitness']])
+        return lesser + [pivot] + greater
     
-    def do_elitism(self):
-        return []
+    def _process_population(self,generation_index):
+        '''
+        Process the population
+        making self._individual_benchmark_rank and self._individual_total_fitness
+        '''
+        self._individual_benchmark_rank = {}
+        self._individual_total_fitness = {}
+        for benchmark in self._benchmarks:
+            self._individual_benchmark_rank[benchmark] = []
+            self._individual_total_fitness[benchmark] = 0.0
+            for individual_index in self._generations[generation_index]:
+                individual_benchmark = {}
+                individual_benchmark['index'] = individual_index
+                individual_benchmark['fitness'] = self._fitness[individual_index][benchmark]
+                self._individual_benchmark_rank[benchmark].append(individual_benchmark)
+                self._individual_total_fitness += self._fitness[individual_index][benchmark]
+            # quick sort
+            self._individual_benchmark_rank[benchmark] = self._sort(self,benchmark)
+            
     
-    def do_mutation(self,individual):
-        return {}
+    def _get_individual_indexes(self,benchmark='default'):
+        # take random individual (roulette wheel scenario for a benchmark)
+        num = random.random() * self._individual_total_fitness[benchmark]
+        acc = 0
+        for i in xrange(self._individual_benchmark_rank):
+            acc += self._individual_benchmark_rank[i]['fitness']
+            if acc>= num:
+                return self._individual_benchmark_rank['index']
     
-    def do_crossover(self,individual_1, individual_2):
-        return {}
+    def _get_elite_individual_indexes(self,count,benchmark='default'):
+        individuals = []
+        for i in xrange(count):
+            individuals.append(self._individual_benchmark_rank[benchmark][i]['index'])
+        return individuals
     
-    def do_process_individual(self,individual):
-        return {}
+    def _add_to_generation(self,individual_index, generation_index=0):
+        if generation_index < (len(self._generations)-1):
+            self._generations[generation_index] = []
+        self._generations[generation_index].append(individual_index)
+        
     
-    def calculate_fitness(self,individual):
-        return {'default':0}
-    
-    def register_individual(self,individual):
+    def _register_individual(self,individual, generation_index=0):
+        # completing individual representation
         individual = self._do_process_individual(individual)
-        try:          
-            return self._individuals.index(individual)            
+        try:    
+            # add to generation      
+            individual_index = self._individuals.index(individual)
+            self._add_to_generation(individual_index, generation_index)            
         except:
+            # calculation fitness
             fitness = self._do_calculate_fitness(individual)
+            # register individual and fitness as 'already calculated'
+            # so we don't need to calculate it again whenever meet such an individual
             self._individuals.append(individual)
             self._fitness.append(fitness)
-            return self._individuals.index(individual)
+            # add to generation
+            individual_index = self._individuals.index(individual)
+            self._add_to_generation(individual_index, generation_index)
     
     def process(self):
         total_rate = self._elitism_rate + self._mutation_rate + self._crossover_rate + self._new_rate
         elitism_count = int(self._population_size * self._elitism_rate/total_rate)
         mutation_count = elitism_count + int(self._population_size * self._mutation_rate/total_rate)
         crossover_count = mutation_count + int(self._population_size * self._crossover_rate/total_rate)
+        elite_individual_per_benchmark = elitism_count/len(self._benchmarks) or 1
+        mutation_individual_per_benchmark = mutation_count/len(self._benchmarks) or 1
+        crossover_individual_per_benchmark = crossover_count/len(self._benchmarks) or 1
         # for every generation
         gen = 0
         while gen<self._max_epoch:
-            # for every population of generation
-            pop = 0
-            while pop<self._population_size:
-                if pop<elitism_count:
-                    pass
-                elif pop<mutation_count:
-                    pass
-                elif pop<crossover_count:
-                    pass
-                else:
-                    pass
-                pop+=1
+            if gen>0:
+                # for every benchmark
+                for benchmark in self._benchmarks:
+                    # _get_elite_individual_indexes
+                    individual_indexes = self._get_elite_individual_indexes(elite_individual_per_benchmark, benchmark)
+                    for individual_index in individual_indexes:
+                        self._add_to_generation(individual_index, gen)
+                    # mutation
+                    for i in xrange(mutation_individual_per_benchmark):
+                        individual = self._get_individual_indexes(benchmark)
+                        new_individual = self.do_mutation(individual)
+                        self._register_individual(new_individual, gen)
+                    # crossover
+                    for i in xrange(int(crossover_individual_per_benchmark/2) or 1):
+                        individual_1 = self._get_individual_indexes(benchmark)
+                        individual_2 = self._get_individual_indexes(benchmark)
+                        new_individual_1, new_individual_2 = self.do_crossover(individual_1, individual_2)
+                        self._register_individual(new_individual_1, gen)
+                        self._register_individual(new_individual_2, gen)
+            i = len(self._generations[gen])
+            # fill out the current generation with new individuals
+            while i<self._population_size:
+                individual = self.do_generate_new_individual()
+                self._register_individual(individual, gen)
+                i+=1
+            # process the population
+            self._process_population(gen)
+                
             gen+=1
     
     def show(self):
@@ -171,6 +234,40 @@ class GA_Base(object):
         sp_3.set_x_label('Generation')
         sp_3.plot(generation_index, variations)
         fig.show()
+    
+    def do_generate_new_individual(self):
+        '''
+        This method should return a new individual
+        '''
+        raise NotImplementedError
+        return {}
+    
+    def do_mutation(self,individual):
+        '''
+        This method should return an individual after mutation
+        '''
+        raise NotImplementedError
+        return {}
+    
+    def do_crossover(self,individual_1, individual_2):
+        '''
+        This method should return 2 individuals after crossover process
+        '''
+        raise NotImplementedError
+        return []
+    
+    def do_process_individual(self,individual):
+        '''
+        This method should return complete individual
+        '''
+        raise NotImplementedError
+        return {}
+    
+    def do_calculate_fitness(self,individual):
+        '''
+        This method should return fitness of individual
+        '''
+        return {'default':0}
     
     @property
     def individuals(self):
@@ -244,5 +341,4 @@ class GA_Base(object):
         if type(value) == list:
             self._benchmarks = value
         else:
-            self._benchmarks.append(value)
-        
+            self._benchmarks.append(value)        
--- GA.py
+++ GA.py
@@ -3,18 +3,28 @@ Created on Oct 23, 2012
 
 @author: gofrendi
 '''
-from GA_Base import GA_Base
+from Base import GA_Base
 
 class GA(GA_Base):
     '''
     Genetics Algorithm class
     '''
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
-        GA_Base.__init__(self)
+    def do_process_individual(self, individual):
+        return GA_Base.do_process_individual(self, individual)
+        
+    def do_calculate_fitness(self, individual):
+        return GA_Base.do_calculate_fitness(self, individual)
+    
+    def do_generate_new_individual(self):
+        return GA_Base.do_generate_new_individual(self)
+    
+    def do_crossover(self, individual_1, individual_2):
+        return GA_Base.do_crossover(self, individual_1, individual_2)
+    
+    def do_mutation(self, individual):
+        return GA_Base.do_mutation(self, individual)
+    
+    
 
 if __name__ == '__main__':
     ga = GA()
