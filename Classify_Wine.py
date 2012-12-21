7e3ed0c6cdb006270f8b1062ad20631b03701060
Go Frendi Gunawan <goFrendiAsgard@gmail.com>
2012-12-21 16:32:54 +0700

    fix delimiter for wine data

--- Classify_Wine.py
+++ Classify_Wine.py
@@ -1,7 +1,7 @@
 from FE_Base import feature_extracting, extract_csv
 
 # make feature extractor
-attribute = extract_csv('wine.data.csv', delimiter=';')
+attribute = extract_csv('wine.data.csv', delimiter=',')
 variables = attribute['variables']
 records = attribute['data']
 feature_extracting(records, variables, label='Wine Data', fold=10)
