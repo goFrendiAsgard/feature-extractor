ed90b323330286a69db05c64e37e413f2ee076bc
goFrendiAsgard <goFrendiAsgard@gmail.com>
2013-02-14 23:01:37 +0700

    try to use "tatami" scenario

--- tatami.py
+++ tatami.py
@@ -1,5 +1,39 @@
 from Feature_Extractor import *
 
+class New_GE(GE_Multi_Accuration_Fitness):
+    def process(self):
+        training_data = self.training_data
+        training_label_target = self.training_label_target
+        #training_num_target = self.training_num_target
+        records = list(training_data)
+        for i in xrange(len(records)):
+            records[i].append(training_label_target)
+        best_phenotypes = []
+        ommited_classes = []
+        while(len(training_data)>1):
+            fe = GE_Multi_Accuration_Fitness(records, fold_count, 0)
+            fe.max_epoch=50
+            fe.process()
+            # look for best benchmark in this iteration
+            best_fitness = 0
+            best_benchmark = ''
+            best_phenotype = ''
+            for benchmark in fe.benchmarks:
+                phenotype = fe.best_individuals(1, benchmark, 'phenotype')
+                fitness = fe.best_fitnesses(1, benchmark)
+                # look for the best one
+                if fitness>best_fitness:
+                    best_fitness = fitness
+                    best_benchmark = benchmark
+                    best_phenotype = phenotype
+            # get best_phenotypes, and omitted_classes
+            best_phenotypes.append(best_phenotype)
+            ommited_classes.append(best_benchmark)
+            # remove the class from the record
+            new_records = [record for record in records if not (record[len(record)-1] == best_benchmark) or record==records[0] ]
+            records = new_records
+        # we have best phenotype here, what to do?
+
 records = extract_csv('iris.data.csv', delimiter=',')
 records = shuffle_record(records)
 old_records = list(records)
