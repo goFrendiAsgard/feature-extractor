07daf95a8da7edef6be55480e0c0d859fd913db8
GoFrendi Gunawan <gofrendiasgard@gmail.com>
2013-05-11 10:25:44 +0700

    try to add "gravilis" scenario, memory exhausted

--- Classify.py
+++ Classify.py
@@ -13,13 +13,13 @@ def extract_feature(csv_file_name, label = None):
     # define classifier    
     classifier = DecisionTreeClassifier(max_depth=group_count-1, random_state=0)
     # define extractors
-    params = {'max_epoch':100,'population_size':100, 'mutation_rate':0.25, 'new_rate':0.5, 
+    params = {'max_epoch':50,'population_size':25, 'mutation_rate':0.25, 'new_rate':0.5, 
               'elitism_rate':0.05, 'crossover_rate': 0.2, 'stopping_value':1.0}
     extractors = [
-        {'class': FE.GA_Select_Feature, 'label':'GA Select Feature', 'color':'red', 'params':params},        
-        {'class': FE.GE_Global_Separability_Fitness, 'label':'GE Global', 'color':'blue', 'params':params},
-        {'class': FE.GE_Multi_Accuration_Fitness, 'label':'GE Multi', 'color':'cyan', 'params':params},
-        {'class': FE.GE_Tatami_Multi_Accuration_Fitness, 'label':'GE Tatami Multi', 'color':'magenta', 'params':params},
+        #{'class': FE.GA_Select_Feature, 'label':'GA Select Feature', 'color':'red', 'params':params},        
+        #{'class': FE.GE_Global_Separability_Fitness, 'label':'GE Global', 'color':'blue', 'params':params},
+        #{'class': FE.GE_Multi_Accuration_Fitness, 'label':'GE Multi', 'color':'cyan', 'params':params},
+        #{'class': FE.GE_Tatami_Multi_Accuration_Fitness, 'label':'GE Tatami Multi', 'color':'magenta', 'params':params},
         {'class': FE.GE_Gravalis, 'label':'GE Gravalis', 'color':'green','params':params},
     ]
     # get label
--- table_maker.py
+++ table_maker.py
@@ -1,14 +1,25 @@
-dir_name = 'RESULT/ecoli.edit (5 fold)'
-label_list = ['GA Select Feature', 'GE Global', 'GE Multi', 'GE Tatami Multi']
+data_name = 'iris.data'
 fold_count = 5
+if fold_count == 1:
+    dir_name = 'RESULT/'+data_name+' (whole)'
+else:
+    dir_name = 'RESULT/'+data_name+' ('+str(fold_count)+' fold)'
+label_list = ['GA Select Feature', 'GE Global', 'GE Multi', 'GE Tatami Multi', 'GE Gravalis']
 
 import os
 
 # make table head
-thead = '<td colspan="2">Percobaan</td>'
+thead = '<tr>'
+thead += '<th rowspan="2" colspan="2">Experiment</th>'
 for label in label_list:
-    thead += '<td>'+label+'</td>'
-thead = '<thead><tr>'+thead+'</tr></thead>'
+    thead += '<th colspan="2">'+label+'</th>'
+thead += '</tr>'
+thead += '<tr>'
+for label in label_list:
+    thead += '<th>Acc</th>'
+    thead += '<th>F. Count</th>'
+thead += '</tr>'
+thead = '<thead>'+thead+'</thead>'
 
 # make table body
 tbody = ''
@@ -18,14 +29,17 @@ for fold_index in xrange(1,fold_count+1):
         file_name = os.path.join(dir_name, label+' Fold '+str(fold_index)+'.txt')
         fh = open(file_name)
         accuracy = []
+        feature_used = 0
         for lino, line in enumerate(fh):
-            if lino>4:
+            if lino>7:
                 break
-            elif lino >= 2:
+            elif lino >= 2 and lino<=4:
                 value = float(line.split(':')[1].lstrip()) * 100.0
                 value = round(value, 2)
                 accuracy.append(str(value)+'%')
-        training_row += '<td>'+accuracy[0]+'</td>'
+            elif lino == 7:
+                feature_used = str(int(line.split(' ')[0]))
+        training_row += '<td>'+accuracy[0]+'</td><td rowspan="3">'+feature_used+'</td>'
         testing_row += '<td>'+accuracy[1]+'</td>'
         total_row += '<td>'+accuracy[2]+'</td>'
         fh.close()
@@ -35,7 +49,7 @@ for fold_index in xrange(1,fold_count+1):
     total_row = '<tr><td>Total</td>' + total_row + '</tr>'
     tbody += training_row + testing_row + total_row
 
-html = '<table>'+thead+tbody+'<table>'
+html = '<table border="1">'+thead+tbody+'<table>'
 # write the file
 html_file_name = os.path.join(dir_name,'resume.html')
 fh = open(html_file_name, 'w')
